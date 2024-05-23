2983badeab668dff226c848396ec8e10781120a0
Austin Rafuls <33244304+arafuls@users.noreply.github.com>
2024-05-23 18:49:11 -0400

    Added versioning.

--- WPF-ROSE-Online-Login-Manager/AssemblyInfo.cs
+++ WPF-ROSE-Online-Login-Manager/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +9,9 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: AssemblyTitle("ROSE Online Login Manager")]
+[assembly: AssemblyDescription("An automation tool for logging  into ROSE Online.")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
