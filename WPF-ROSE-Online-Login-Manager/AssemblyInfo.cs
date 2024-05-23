69ad9c7a1cc27cb62df4fa446e824e61f46d506e
Austin Rafuls <33244304+arafuls@users.noreply.github.com>
2024-05-23 19:23:38 -0400

    Remove redundant versioning.

--- WPF-ROSE-Online-Login-Manager/AssemblyInfo.cs
+++ WPF-ROSE-Online-Login-Manager/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -9,9 +8,3 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
-
-[assembly: AssemblyTitle("ROSE Online Login Manager")]
-[assembly: AssemblyDescription("An automation tool for logging  into ROSE Online.")]
-
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
